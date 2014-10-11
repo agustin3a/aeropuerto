7f0de38a61be7d837216fbf605fb70666f2260ea
Carlos Aguilar <agustin3a94@gmail.com>
2014-10-10 23:13:22 -0600

    Screen real time

--- js/vuelos.js
+++ js/vuelos.js
@@ -1,15 +1,32 @@
 (function() {
   var app = angular.module('vuelos', []);
-  app.controller("arrivalsController",function($scope,$http) {
-	  $http.get('/aeropuerto/src/flights.php').
-	  success(function(data) {
-	    $scope.arrivals = data;
-	  });
+  app.controller("arrivalsController",function($scope,$http,$timeout) {
+	  $scope.arrivals = {};
+    var poll = function() {
+        $timeout(function() {
+            $http.get('/aeropuerto/src/flights.php').
+	  				success(function(data) {
+	    			$scope.arrivals = data;
+	  				});
+            poll();
+        }, 1000);
+    };     
+   	poll();
+
   });
-  app.controller("departuresController",function($scope,$http) {
-    $http.get('/aeropuerto/src/flights.php').
-    success(function(data) {
-      $scope.departures = data;
-    });
+
+
+  app.controller("departuresController",function($scope,$http,$timeout) {
+    $scope.departures = {};
+    var poll = function() {
+        $timeout(function() {
+            $http.get('/aeropuerto/src/flights.php').
+	  				success(function(data) {
+	    			$scope.departures = data;
+	  				});
+            poll();
+        }, 1000);
+    };     
+   	poll();
   });
 })();
